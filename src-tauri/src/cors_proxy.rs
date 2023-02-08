048375119443c56d28b30eb040f6fa2a37fe269f
Daxe9 <davidexie694@gmail.com>
2023-02-08 22:12:50 +0100

    removed duration and status from api request

--- src-tauri/src/cors_proxy.rs
+++ src-tauri/src/cors_proxy.rs
@@ -1,6 +1,6 @@
 // TODO: share client for all requests
-use reqwest::{header::{HeaderValue, HeaderMap}, StatusCode};
-use std::{time::{Instant, Duration}, collections::HashMap};
+use reqwest::header::{HeaderValue, HeaderMap};
+use std::collections::HashMap;
 use serde::Serialize;
 use serde_json::Value;
 
@@ -8,22 +8,16 @@ use serde_json::Value;
 pub struct ResponseData {
     body: String,
     headers: HashMap<String, String>,
-    status_code: u16,
-    duration: u64
 }
 
 impl ResponseData {
-    pub fn new(tmp_headers: HeaderMap, status_code: StatusCode, duration: Duration, body: String) -> Self {
+    pub fn new(tmp_headers: HeaderMap, body: String) -> Self {
         let mut headers: HashMap<String, String> = HashMap::new(); 
         for (key, value) in tmp_headers.iter() {
             headers.insert(key.to_string(), value.to_str().unwrap().to_string());
         }
-        let status_code = status_code.as_u16();
-        let duration: u64 = duration.as_micros() as u64;
         Self {
-            duration,
             headers,
-            status_code,
             body
         }
     }
@@ -55,22 +49,15 @@ pub async fn get_request(url: &str, headers: Option<HashMap<&str, &str>>) -> Res
 
     let client = reqwest::Client::new();
 
-    // measure api call time
-    let start_timer = Instant::now();
     // make get request
     let raw_result = client.get(url).headers(header_map).send().await?;
-    // end  the timer
-    let duration = start_timer.elapsed();
 
     // get complementary data
     let headers = raw_result.headers().clone();
-    let status = raw_result.status();
 
     // create the response data to send back to the frontend
     let response_data = ResponseData::new(
         headers,
-        status,
-        duration,
         raw_result.text().await?,
     );
 
@@ -109,25 +96,18 @@ pub async fn post_request(url: &str, body: &str, headers: Option<HashMap<&str, &
         // inizialize the client
     let client = reqwest::Client::new();
     
-    // measure api call time
-    let start_timer = Instant::now();
     // make the post request with given body
     let raw_result = match client.post(url).headers(header_map).json(&body).send().await {
         Ok(v) => v,
         Err(err) => return Err(APIError::RequestError(err))
     };
-    // end  the timer
-    let duration = start_timer.elapsed();
 
     // get complementary data
     let headers = raw_result.headers().clone();
-    let status = raw_result.status();
     
     // create the response data to send back to the frontend
     let response_data = ResponseData::new(
         headers,
-        status,
-        duration,
         raw_result.text().await?,
         );
 
