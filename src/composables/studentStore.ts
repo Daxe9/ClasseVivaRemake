78c5ecbf0c90f4acb5f01725547d586fb33c02a9
Daxe9 <davidexie694@gmail.com>
2023-02-14 17:28:13 +0100

    added callback for data handling

--- src/composables/studentStore.ts
+++ src/composables/studentStore.ts
@@ -1,14 +1,21 @@
 import { useStudentInfoStore } from "@/stores/studentInfo";
 import { ref } from "vue";
 
-export function subscribeTo(property: string) {
-	const studentInfo = useStudentInfoStore();
+const studentInfo = useStudentInfoStore();
+
+// It takes an optional callback function to process the subscribed data
+export function subscribeTo(property: string, callback?: (value: any) => any) {
 	// @ts-ignore
 	const result = ref(studentInfo[property]);
 
 	studentInfo.$subscribe((_, state) => {
-		// @ts-ignore
-		result.value = state[property];
+		if (callback) {
+			// @ts-ignore
+			result.value = callback(state[property]);
+		} else {
+			// @ts-ignore
+			result.value = state[property];
+		}
 	});
 
 	return result;
